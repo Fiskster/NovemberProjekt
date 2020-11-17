9ab716758ce67ece5de1a53510a13dfb3f3c6966
Axel <axel.lilja@elev.ga.ntig.se>
2020-11-17 10:51:25 +0100

    First Commit

---
+++ novemberProjekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Raylib_cs;
+namespace novemberProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Planen är simpel, Ha en slags hub som finns i rpg-spel i starwars-tema där man kan interacta med saker som uppfyller alla kraven på A-nivå.
+            //Detta då i star wars tema :) 
+            Console.WriteLine("Poggers!");
+            
+        }
+    }
+}
