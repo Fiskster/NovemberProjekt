c73f090d02e91f34f7935c803648de40637f11d3
Axel <axel.lilja@elev.ga.ntig.se>
2020-11-18 11:09:40 +0100

    Raylib and main character

--- novemberProjekt/Program.cs
+++ novemberProjekt/Program.cs
@@ -8,8 +8,29 @@ namespace novemberProjekt
         {
             //Planen är simpel, Ha en slags hub som finns i rpg-spel i starwars-tema där man kan interacta med saker som uppfyller alla kraven på A-nivå.
             //Detta då i star wars tema :) 
-            Console.WriteLine("Poggers!");
-            
+            Raylib.InitWindow(800, 600, "Hello World");
+
+            float x = 100;
+            float y = 400;
+
+            var bruh = Raylib.LoadImage(@"C:\Users\axel.lilja2\Documents\PROG02\NovemberProjekt\novemberProjekt\bin\Debug\netcoreapp3.1\MainChar.png");
+            Texture2D texture = Raylib.LoadTextureFromImage(bruh);
+
+
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.BLUE);
+
+              Raylib.DrawTexture(texture, (int)x, (int)y, Color.WHITE);
+
+
+                Raylib.EndDrawing();
+            }
+
+
         }
     }
 }
